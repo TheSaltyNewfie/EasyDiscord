864d7b5ebb9f562dcbf787e86e6ab13ef3105def
TheSaltyNewfie <tobydodge65@gmail.com>
2021-06-08 17:14:49 -0230

    guildMembers can now print the list to a file

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ This is a module for python that has definitions in it to help people code a Dis
 # Who can use this?
 Anyone can! This module is meant for people who are new to python or the Discord API itself and for people who are
 experienced and just want to make their code more pretty.
+# Why did you make this module?
+I wanted to rewrite my current bot and have less lines to make it more
+understandable and I thought it would be good to release to the public
--- init.py
+++ init.py
@@ -13,19 +13,20 @@ from json import *
 intents = discord.Intents.all()
 client = discord.client(intents=intents)
 
-def getVersion(param1, param2, param3):
-    #this is a networking definition for grabbing a version file from
-    #a CDN somewhere on the internet
+def getFile(param1, param2, param3):
+#This definition get a file from any CDN server, can be useful
+#for getting images or videos from discord messages
     findVersion = requests.get(param3 , allow_redirects=True)
     open(param1, param2).write(findVersion.content)
 
 def createFile(Name, Binary, info):
-#Creates a file weither its text or something else as show by the Binary paramater
+#Creates a file weither its text or something else as show by 
+#the Binary paramater
     with open(Name, Binary) as writefile:
         print(f'{info}', file=writefile)
 
 def compareFile(file1, file2, ShowDif):
-    #This definition will compare 2 files selected by the user
+#This definition will compare 2 files selected by the user
     filecmp.cmp(file1, file2)
     if ShowDif == True:
         print(filecmp.cmp(file1, file2)) 
@@ -37,6 +38,8 @@ def readFile(Name, Binary):
         print(readfile, file=readfile)
 
 def getWords(IName, IBinary, OName, OBinary):
+#This definition will grab words from messages and log them 
+#within a defined file
     async def on_message(message):
     
         with open(IName, IBinary) as file:
@@ -48,10 +51,17 @@ def getWords(IName, IBinary, OName, OBinary):
                     print(f"{message.author}: {message.content} \n", file=file)
                     print('{0.author} Word Logged'.format(message))
 
-def guildMembers(GUILD):
-    #This only prints the guild members at startup
+def guildMembers(GUILD, Output):
+#This shows the guild members, could be useful for listing members
+#on a leaderboard
     for guild in client.guilds:
         if guild.name == GUILD:
             break
     members = '\n - '.join([member.name for member in guild.members])
     print(f'Guild Members:\n - {members}')
+
+    if Output == True:
+        with open("GuildMembers.txt", 'w') as file:
+            print(f'{members}', file=file)
+    else:
+        pass
